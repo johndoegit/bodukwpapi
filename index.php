fe1c3a2366d445d2538d74ab25efee5847eddeaa
johndoeedit <johndoeemailaddress69@gmail.com>
2019-02-17 10:05:48 +0800

    good

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
  
 include('./jwt/JwtClass.php'); 
  
 $jwt = new JwtClass();
 
 
+//
+//mysql_real_escape_string
+if(isset($_POST['username']) && isset($_POST['password'])){
+ 
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$check_user = $jwt->jwt_login_check($username,$password);
+	if($check_user === 1){
+		 $jwt->jwt_login($username);
+			$token = array(
+		    "user" => $username,
+		    "time" => time(),
+		    "REMOTE_ADDR" => $_SERVER['REMOTE_ADDR'],
+		    "SERVER_ADDR" => $_SERVER['SERVER_ADDR'],
+		    "REQUEST_TIME" => $_SERVER['REQUEST_TIME']
+		);
+		echo $jwt->jwt_encode($token);
+	}else{
+		echo 'wrong username or password';
+	}
+	
+}
 
 // //check username and password
 // $check_user = $jwt->jwt_login_check('root','admin');
@@ -24,6 +46,7 @@ $jwt = new JwtClass();
 //$jwt->jwt_logout();
  
  
+//$jwt->jwt_decode('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoicm9vdCIsInRpbWUiOjE1NTAyNzkyMDUsIlJFTU9URV9BRERSIjoiOjoxIiwiU0VSVkVSX0FERFIiOiI6OjEiLCJSRVFVRVNUX1RJTUUiOiIxNTUwMjc5MjA1In0.SMH2jlxkU7_LHIK1nD3mswjA-q30ma2_lI7UaF8YtfU'); 
 
  
 
--- jwt/JwtClass.php
+++ jwt/JwtClass.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 include_once './jwt/src/BeforeValidException.php';
 include_once './jwt/src/ExpiredException.php';
@@ -17,46 +17,35 @@ class JwtClass {
 const key = "rewtre@$#WTEGDFBtrjhgew23rwetrhgnfger3436574yerhgs(*&^dfsfs";
 
  
-public function jwt_encode(){
+public function jwt_encode($data){
 	
-		$token = array(
-	    "user_id" => "001",
-	    "user_email" => "001",
-	    "time" => time(),
-	    "REMOTE_ADDR" => $_SERVER['REMOTE_ADDR'],
-	    "SERVER_ADDR" => $_SERVER['SERVER_ADDR'],
-	    "REQUEST_TIME" => $_SERVER['REQUEST_TIME']
-	);
+	$token = $data;
 	
 	$jwt = JWT::encode($token, self::key);
 	$decoded = JWT::decode($jwt, self::key, array('HS256'));
 
-//print_r($decoded);
- 
-return print_r($jwt, true);
-//echo '<br>';
-//echo print_r($decoded->user_id);	
+	//print_r($decoded);
+	 
+	return print_r($jwt, true);
+	//echo '<br>';
+	//echo print_r($decoded->user_id);	
 }
 
-public function jwt_decode(){
+public function jwt_decode($data){
 	
-		$token = array(
-	    "user_id" => "001",
-	    "user_email" => "001",
-	    "time" => time(),
-	    "REMOTE_ADDR" => $_SERVER['REMOTE_ADDR'],
-	    "SERVER_ADDR" => $_SERVER['SERVER_ADDR'],
-	    "REQUEST_TIME" => $_SERVER['REQUEST_TIME']
-	);
-	
-	$jwt = JWT::encode($token, self::key);
-	$decoded = JWT::decode($jwt, self::key, array('HS256'));
-
-return print_r($decoded);
  
-///return print_r($jwt, true);
-//echo '<br>';
-//echo print_r($decoded->user_id);	
+	$decoded = JWT::decode($data, self::key, array('HS256'));
+
+	if($decoded){
+		return print_r($decoded);	
+	}else{
+		return 'error!';
+	}
+	
+	 
+	///return print_r($jwt, true);
+	//echo '<br>';
+	//echo print_r($decoded->user_id);	
 }
 
 public function jwt_login($username){
